22841722c652c4cd874d5265dc3be61838f34d8b
Matthew Lee <mlee212@ucr.edu>
2019-11-25 15:12:36 -0800

    Parser almost fully debugged of parenthesis

--- src/input_data.cpp
+++ src/input_data.cpp
@@ -14,6 +14,10 @@ void InputData::takeInput() {
 //	cout << "$";
 //	getline(cin, str);
 	
+	while (str.at(0) == '(' && str.at(str.length() - 1) == ')') {
+		str = str.substr(1, str.length() - 2);
+	}
+	
 	bool quote = false;
 	//cout << "og str:" << str << endl;
 	for(int i = 0; i < str.size(); i++) {
@@ -56,7 +60,7 @@ void InputData::takeInput() {
 	int exeLength = 0;
 	string temp;
 	quote = false;
-	bool paren = false;
+	int paren = 0;
 	bool quoteSec = false;
 	bool quit = false;
 	bool quitCheck = false;
@@ -69,15 +73,15 @@ void InputData::takeInput() {
 	stringstream ss(str);
 	while(ss >> temp) {
 		
-		if(temp.at(0) == '(') {
-			paren = true;
-		}
-		if(temp.at(temp.size() - 1) == ')') {
-			paren = false;
-		}
 		for(int i = 0; i < temp.size(); i++) {
 			if (temp.at(i) == '\"') {
 				quote = !quote;
+			}
+			if(temp.at(i) == '(' && !quote) {
+				paren++;
+			}
+			if(temp.at(i) == ')' && !quote) {
+				paren--;
 			}
 		}
 		if(quote){
@@ -112,9 +116,10 @@ void InputData::takeInput() {
 			indexE += 1;
 			exeLength += 1;
 		}
-		else if(paren){
+		else if(paren > 0){
 		//	quote = true;
 		//	quoteSec = false;
+			cout << "Paren length: " << paren << endl;
 			if(start) {
 				for(int i = 0; i < temp.size(); i++) {
 					indexE++;
@@ -278,6 +283,11 @@ void InputData::takeInput() {
 	//cout << str.size() - 1<< endl;	
 	for(int i = 0; i < inputs.size(); i++) {
 		cout << inputs.at(i)->input << endl;
+		while(inputs.at(i)->input.at(0) == '(' && inputs.at(i)->input.at(inputs.at(i)->input.length() - 1) == ')') {
+			inputs.at(i)->input = inputs.at(i)->input.substr(1, inputs.at(i)->input.length() - 2);
+		}
+		cout << "After: " << inputs.at(i)->input << endl;
+		
 	}
 	cout << "size: " << inputs.size() << endl;
 	//cout << "hello" << endl;
--- src/parenthesis.cpp
+++ src/parenthesis.cpp
@@ -3,7 +3,12 @@
 Parenthesis::Parenthesis(string str) : Input(str) { } 
 
 int Parenthesis::run() {
-	string strToParse = input.substr(1, input.size() - 2);
+	string strToParse = input;
+	while (strToParse.at(0) == '(' && strToParse.at(input.length() - 1) == ')') {
+		strToParse = strToParse.substr(1, input.size() - 2);
+	}	
+//	string strToParse = input.substr(1, input.size() - 2);
+	cout << "strToParse: " << strToParse << endl;
 	InputData exeList = InputData(strToParse);
 	exeList.takeInput();
 	return exeList.run();
