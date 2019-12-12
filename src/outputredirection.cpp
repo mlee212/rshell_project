26f4a82e891ed9b2d56243bf6069e08100d19cb4
Andrew Nguyen <nguyena8047@gmail.com>
2019-12-12 08:48:43 -0800

    Fixed errors in redirectors

--- src/outputredirection.cpp
+++ src/outputredirection.cpp
@@ -1,7 +1,7 @@
 #include "../header/outputredirection.hpp"
 
 int OutputRedirection::run() {
-    int savetstdout = dup(1);
+    int savestdout = dup(1);
     int file = open(right->input.c_str(),  O_WRONLY | O_CREAT | O_TRUNC, 0664);
     dup2(file, 1);
     if (left->run() == -1){
