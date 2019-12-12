da6a9a0bb483b45a03929af4fd2e1b702b9042d5
Andrew Nguyen <nguyena8047@gmail.com>
2019-12-12 09:13:21 -0800

    Removed new testing tools

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -12,22 +12,22 @@ int Pipe::run() {
     int x = p[1];
     int y = p[0];
 
-    cout << "hello1 dup(0): "
+    cout << "hello1 dup(0): " << endl;
     dup2(x, 1);
-    cout << "hello2 dup(0): "
+    cout << "hello2 dup(0): " << endl;
     if (left->run() == -1){
         return -1;
     }
-    cout << "hello3 dup(0): "
+    cout << "hello3 dup(0): " << endl;
     dup2(savestdout, 1);
     close(x);
-    cout << "hello4 dup(0): "
+    cout << "hello4 dup(0): " << endl;
     dup2(y, 0);
-    cout << "hello5 dup(0): " << dup(0) << " dup(1): " << dup(1) << endl;
+    cout << "hello5 dup(0): " << endl;
     if (right->run() == -1){
         return -1;
     }
-    cout << "hello6 dup(0): " << dup(0) << " dup(1): " << dup(1) << endl;
+    cout << "hello6 dup(0): " << endl;
     dup2(savestdin, 0);
     close(y);
 
