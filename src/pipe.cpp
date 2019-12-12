256965c763299178233dc9595e89108218725c6a
Andrew Nguyen <nguyena8047@gmail.com>
2019-12-12 09:04:36 -0800

    Added testing tools

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -12,22 +12,22 @@ int Pipe::run() {
     int x = p[1];
     int y = p[0];
 
-    // cout << "hello1" << endl;
+    cout << "hello1" << endl;
     dup2(x, 1);
-    // cout << "hello2" << endl;
+    cout << "hello2" << endl;
     if (left->run() == -1){
         return -1;
     }
-    // cout << "hello3" << endl;
+    cout << "hello3" << endl;
     dup2(savestdout, 1);
     close(x);
-    // cout << "hello4" << endl;
+    cout << "hello4" << endl;
     dup2(y, 0);
-    // cout << "hello5" << endl;
+    cout << "hello5" << endl;
     if (right->run() == -1){
         return -1;
     }
-    // cout << "hello6" << endl;
+    cout << "hello6" << endl;
     dup2(savestdin, 0);
     close(y);
 
