6855dc0086ea0eb2db87948a62f011f1b9ecf3ee
Andrew Nguyen <nguyena8047@gmail.com>
2019-12-12 08:57:36 -0800

    Allowed for 2+ redirectors

--- src/outputredirection.cpp
+++ src/outputredirection.cpp
@@ -1,7 +1,9 @@
 #include "../header/outputredirection.hpp"
+#include "../header/executable.hpp"
 
 int OutputRedirection::run() {
     int savestdout = dup(1);
+    left = new Executable(input);
     int file = open(right->input.c_str(),  O_WRONLY | O_CREAT | O_TRUNC, 0664);
     dup2(file, 1);
     if (left->run() == -1){
