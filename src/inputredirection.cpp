dd2f1bafbbada8f9aa12999a372181866f485b26
Andrew Nguyen <nguyena8047@gmail.com>
2019-12-12 08:45:45 -0800

    Fixed errors in redirectors

--- src/inputredirection.cpp
+++ src/inputredirection.cpp
@@ -1,9 +1,10 @@
 #include "../header/inputredirection.hpp"
+#include "../header/executable.hpp"
 
 int InputRedirection::run() {
     int savestdin = dup(0);
     left = new Executable(left->input + " " + right->input);
-    file = open(right->input, O_RDONLY | O_EXCL);
+    int file = open(right->input, O_RDONLY | O_EXCL);
     if (file == -1) {
         return -1;
     }
